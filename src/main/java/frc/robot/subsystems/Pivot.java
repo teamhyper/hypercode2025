15bcb4564e8b753a8c9f20e8d4f1be54b7672aea
Chris McGroarty <iammarix@gmail.com>
2025-03-02 16:41:48 -0500

    add offset positions to the Pivot

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -17,20 +17,18 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pivot extends SubsystemBase {
+    public static final double COLLECT_CORAL_POSITION_OFFSET = 0.0, COLLECT_ALGAE_POSITION_OFFSET = 0.0, SCORE_ALGAE_POSITION_OFFSET = 0.0, SCORE_CORAL_POSITION_OFFSET = 0.0;
     // PID coefficients
     // from REV example p = 0.1, i = 1e-4, d = 1
     private static final double kP = 0.0075, kI = 0.0, kD = 0.0;
     private static final double kFF = 0.0, kMinOutput = -1.0, kMaxOutput = 1.0;
-
     // Max Motion parameters
     private static final double kMaxVelocity = 1000.0, kMaxAcceleration = 500.0, kAllowedError = 0.005;
-
     // REV soft limit parameters
     private static final boolean kForwardSoftLimitEnabled = true, kReverseSoftLimitEnabled = true;
-    private static final double kReverseSoftLimit = 212.5, kForwardSoftLimit = 308.5;
-
     private static final int PIVOT_MOTOR_ID = 20;
     private static final double kS = 0, kG = 0, kV = 0, kA = 0;
+    private static final double ALL_IN_POSITION = 212.5, ALL_OUT_POSITION = 308.5;
     private final SparkMax m_pivot;
     private final SparkAbsoluteEncoder encoder;
     private final SparkClosedLoopController pidController;
@@ -68,54 +66,111 @@ public class Pivot extends SubsystemBase {
         config.softLimit
                 .forwardSoftLimitEnabled(kForwardSoftLimitEnabled)
                 .reverseSoftLimitEnabled(kReverseSoftLimitEnabled)
-                .reverseSoftLimit(kReverseSoftLimit)
-                .forwardSoftLimit(kForwardSoftLimit);
+                .reverseSoftLimit(ALL_IN_POSITION)
+                .forwardSoftLimit(ALL_OUT_POSITION);
 
         m_pivot.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         pidController = m_pivot.getClosedLoopController();
         encoder = m_pivot.getAbsoluteEncoder();
 
-        setDefaultCommand(this.setTargetPositionCommand().andThen(
-                new RunCommand(() -> {
-                }, this)));
+        setDefaultCommand(setPositionAndHoldCommand());
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("EncoderPosition", this.getPosition());
+        SmartDashboard.putNumber("Pivot: EncoderPosition (Actual)", getPosition());
+        SmartDashboard.putNumber("Pivot: EncoderPosition (Offset)", getPosition() - ALL_IN_POSITION);
+        SmartDashboard.putBoolean("Pivot: CanMove In", getPosition() > ALL_IN_POSITION);
+        SmartDashboard.putBoolean("Pivot: CanMove Out", getPosition() < ALL_OUT_POSITION);
     }
 
-    // Method to get the current position
+    /**
+     * Method to get the current position
+     *
+     * @return - current position of the absolute encoder
+     */
     public double getPosition() {
         return encoder.getPosition();
     }
 
-    // Method to set the pivot position using Max Motion Profiling
+    /**
+     * Method to set the pivot position using Max Motion Profiling
+     *
+     * @param position - target value for the PID
+     */
     public void setPosition(double position) {
         pidController.setReference(position, SparkMax.ControlType.kMAXMotionPositionControl);
     }
 
-    // Method to stop the pivot
+    public Command setPositionAndHoldCommand() {
+        return setTargetPositionCommand().andThen(
+                new RunCommand(() -> {
+                }, this));
+    }
+
+    /**
+     * set the target position for the PID based on the given offset from the base position
+     *
+     * @param offset - distance from the base position
+     */
+    public void setPositionOffset(double offset) {
+        setPosition(offset + ALL_IN_POSITION);
+    }
+
+    /**
+     * stop the motor (speed 0)
+     */
     public void stop() {
         m_pivot.stopMotor();
     }
 
-    // Command to move pivot manually while a button is held
+    /**
+     * Command to move pivot manually outward while a button is held
+     *
+     * @param speed - speed to run the pivot motor at
+     * @return command to run the motor at positive speed
+     */
     public Command runPivotOut(double speed) {
         return new RunCommand(() -> m_pivot.set(speed), this).finallyDo(interrupted -> stop());
     }
 
-    // Command to move pivot manually while a button is held
+    /**
+     * Command to move pivot manually inward while a button is held
+     *
+     * @param speed - speed to run the pivot motor at
+     * @return command to run the motor at negative speed
+     */
     public Command runPivotIn(double speed) {
         return new RunCommand(() -> m_pivot.set(-speed), this).finallyDo(interrupted -> stop());
     }
 
+    /**
+     * return a command that sets the position to the given offset from the ALL_WAY_IN value
+     *
+     * @param offset - difference from the ALL_WAY_IN position to move
+     * @return command to set the PID setpoint to the given offset from the base position
+     */
+    public Command setTargetPositionOffsetCommand(double offset) {
+        return new InstantCommand(() -> setPositionOffset(offset));
+    }
+
+    /**
+     * set the target on the SparkMax Closed Loop PID to the given position
+     *
+     * @param position - position to move to
+     * @return command to set the PID setpoint to the given position
+     */
     public Command setTargetPositionCommand(double position) {
         return new InstantCommand(() -> setPosition(position));
     }
 
+    /**
+     * set the target on the SparkMax Closed Loop PID to the current position of the absolute encoder
+     *
+     * @return command to set the PID setpoint to the current position
+     */
     public Command setTargetPositionCommand() {
-        return this.setTargetPositionCommand(m_pivot.getAbsoluteEncoder().getPosition());
+        return setTargetPositionCommand(m_pivot.getAbsoluteEncoder().getPosition());
     }
 }
