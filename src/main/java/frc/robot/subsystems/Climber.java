f46be1c942b7eb42ff0c018f94ef48a141a1cd19
luke.vu <luke.m.vu@gmail.com>
2025-03-02 01:10:53 -0500

    WIP add servo init

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -13,6 +13,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,11 +22,18 @@ public class Climber extends SubsystemBase {
     
     private static final int CLIMBER_ID = 19;
     private static final int CLIMBER_ABS_ENC_ID = 24;
+    private static final int CLIMBER_RATCHET_SERVO_PWM_ID = 2; //TODO: pick a port
+    private static final int RAMP_LINEAR_SERVO_PWM_ID = 3; //TODO: pick a port
+
+    private static final double RATCHET_LOCK_POSITION = 1.0; // TODO
+    private static final double RATCHET_UNLOCK_POSITION  = 0.0;
 
     private static final double CLIMBER_ENCODER_OFFSET = 10; // TODO: set when calculated
 
     private final TalonFX m_climber;
     private final CANcoder e_climber;
+    private final Servo s_ratchet;
+    private final Servo s_ramp;
 
     public Climber() {
         this(CLIMBER_ID, CLIMBER_ABS_ENC_ID);
@@ -34,6 +42,8 @@ public class Climber extends SubsystemBase {
     public Climber(int climberID, int climberEncoderID) {
         m_climber = new TalonFX(climberID);
         e_climber = new CANcoder(climberEncoderID, "hyperbus");
+        s_ratchet = new Servo(CLIMBER_RATCHET_SERVO_PWM_ID);
+        s_ramp = new Servo(RAMP_LINEAR_SERVO_PWM_ID);
 
         configMotors();
         configSensors();
@@ -61,13 +71,15 @@ public class Climber extends SubsystemBase {
         CANcoderConfiguration config = new CANcoderConfiguration();
         MagnetSensorConfigs magnetConfig = new MagnetSensorConfigs();
 
-        magnetConfig
+        // magnetConfig
         
     }
 
+    /**
+     * Returns the climber absolute position in degrees.
+     */
     private double getClimberAngle() {
-        // return e_climber.getAbsolutePosition().getValueAsDouble() - CLIMBER_ENCODER_OFFSET;
-        return e_climber.getPosition().getValueAsDouble() - CLIMBER_ENCODER_OFFSET;
+        return e_climber.getPosition().getValueAsDouble() - CLIMBER_ENCODER_OFFSET; // TODO: fix
     }    
 
     /**
@@ -86,9 +98,20 @@ public class Climber extends SubsystemBase {
         m_climber.setControl(new DutyCycleOut(output));
     }
 
+    /**
+     * Set ratchet servo position.
+     * @param position Servo position between 0.0 to 1.0
+     */
+    public void setRatchet(double position) {
+        s_ratchet.set(position);
+    }
+
     @Override
     public void periodic() {
+
       SmartDashboard.putNumber("Climber Angle", getClimberAngle());
+      SmartDashboard.putNumber("Climber Current Output", m_climber.getTorqueCurrent().getValueAsDouble());
+      SmartDashboard.putBoolean("Ratchet Locked", false)
     }
 
     /**
@@ -107,6 +130,6 @@ public class Climber extends SubsystemBase {
         return new RunCommand(() -> runClimberVariable(output.getAsDouble()), this).finallyDo(interrupted -> runClimberVariable(0));
     }
 
-
+    
 
 }
