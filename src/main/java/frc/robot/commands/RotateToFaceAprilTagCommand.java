d5a9774e2905a2d1f3d587990edd917f423eb8df
wue1atwit <wue1@wit.edu>
2025-03-15 18:30:06 -0400

    Added PID controller and semi-tuned it

--- src/main/java/frc/robot/commands/RotateToFaceAprilTagCommand.java
+++ src/main/java/frc/robot/commands/RotateToFaceAprilTagCommand.java
@@ -1,7 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.VisionSubsystem;
@@ -12,12 +16,15 @@ import java.util.List;
 
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 /*
  * A command that rotates the robot to face a specific AprilTag.
  */
 public class RotateToFaceAprilTagCommand extends Command {
     private final VisionSubsystem visionSubsystem;
     private final Drivetrain drivetrain;
+    private final PIDController pidController; 
     private final int targetTag;
     private final double angleTolerance = 5.0; // Adjust as needed
     private boolean isAligned;
@@ -34,6 +41,8 @@ public class RotateToFaceAprilTagCommand extends Command {
         this.drivetrain = drivetrain;
         this.isAligned = false;
         this.targetTag = targetTag;
+        pidController = new PIDController(0.01, 0.08, 0.05);
+        pidController.setTolerance(4.0);
         addRequirements(visionSubsystem, drivetrain);
     }
 
@@ -42,10 +51,24 @@ public class RotateToFaceAprilTagCommand extends Command {
         List<PhotonTrackedTarget> targetList = visionSubsystem.getForwardTags();
         if (targetList.size() > 0) {
             final PhotonTrackedTarget target = targetList.get(targetList.size() - 1);
-            double relativeYaw = target.getYaw();
-            double currentYaw = drivetrain.getPose().getRotation().getDegrees();
-            double targetAbsYaw = currentYaw - relativeYaw;
-            drivetrain.turnToAngle(Rotation2d.fromDegrees(targetAbsYaw));
+            double targetYaw = target.getYaw(); 
+            // double currentYaw = drivetrain.getPose().getRotation().getDegrees();
+            // double targetAbsYaw = currentYaw - relativeYaw;
+            double pidOutput = pidController.calculate(targetYaw, 0);
+
+            SmartDashboard.putNumber("pidOutput", pidOutput);
+            SmartDashboard.putNumber("targetYaw", targetYaw);
+            // SmartDashboard.putNumber("currentYaw", currentYaw);
+            // SmartDashboard.putNumber("targetAbsYaw", targetAbsYaw);
+
+            // Rotation2d rotationCommand = Rotation2d.fromDegrees(pidOutput);
+
+            final var chassisSpeeds = new ChassisSpeeds(0, 0, pidOutput);
+            drivetrain.setControl(new SwerveRequest.ApplyRobotSpeeds().withSpeeds(chassisSpeeds));
+            // drivetrain.applyRequest(null)
+        } else {
+            final var chassisSpeeds = new ChassisSpeeds(0, 0, 0);
+            drivetrain.setControl(new SwerveRequest.ApplyRobotSpeeds().withSpeeds(chassisSpeeds));
         }
     }
 
